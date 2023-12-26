d99c96543ffcad78ccbaecc170b44cb17be1fb88
larva <nguyenthanhcong12gtm1.gmail.com>
2023-12-25 22:00:31 -0800

    Implement simplest of thread pool and test.

--- test/test_thread_pool.cc
+++ test/test_thread_pool.cc
@@ -2,5 +2,15 @@
 #include <thread_pool/thread_pool.hh>
 
 int main() {
+    larva::thread_pool pool;
+
+    for (auto i : {1, 2, 3, 4, 5, 6}) {
+        pool.submit([i]() -> void {
+            std::cout << "Task " << i << " is running." << std::endl;
+        });
+    }
+
+    while (1);
+    
     return EXIT_SUCCESS;
 }
--- thread_manager/thread_pool/thread_pool.hh
+++ thread_manager/thread_pool/thread_pool.hh
@@ -1,11 +1,59 @@
 #pragma once
 #include <atomic>
+#include <functional>
+#include <vector>
+#include <thread>
+
 #include <threadsafe_container/queue.hh>
 
 namespace larva {
 
+typedef std::function<void()> task_t;
+
 class thread_pool {
-    std::atomic_bool _done;
+    std::atomic_bool _done {false};
+    larva::threadsafe_queue<larva::task_t> _work_queue {};
+    std::vector<std::thread> _worker_threads {};
+
+public:
+    thread_pool()
+    {
+        unsigned const thread_number = std::thread::hardware_concurrency();
+        try {
+            for (unsigned i = 0; i < thread_number; ++i)
+            {
+                this->_worker_threads.push_back(
+                            std::thread{&thread_pool::worker_thread, this});
+            }
+        } catch (...) {
+            this->_done = true;
+            throw;
+        }
+    }
+
+    ~thread_pool() {
+        this->_done = true;
+    }
+
+
+    template <typename FunctionType>
+    void submit(FunctionType f)
+    {
+        this->_work_queue.push(larva::task_t(f));
+    }
+
+private:
+    void worker_thread()
+    {
+        while (!this->_done) {
+            larva::task_t task;
+            if (this->_work_queue.try_pop(task)) {
+                task();
+            } else {
+                std::this_thread::yield();
+            }
+        }
+    }   
 };
 
 }
